20faef8c747dd1188bdb6dc7d0e792855271cf8a
Shazia_Idrees <vohragee@gmail.com>
2023-11-11 21:44:46 -0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Shazia Vohra on 2023-11-11.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
